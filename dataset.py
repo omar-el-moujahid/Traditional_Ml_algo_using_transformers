9f6f87190ed972eb5dc72c4b822efe4949dced99
omar-el-moujahid <omarelmoujahid5@gmail.com>
2025-01-23 21:16:22 +0100

    add dataload and tokenization

---
+++ dataset.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn 
+from torch.utils.data  import Dataset
+
+class BilingualDataset(Dataset):
+    def __init__(self , ds , tokenizer_src , tokinezer_tgt , src_lang , tgt_lang , seq_len ):
+        super().__init__()
+        self.ds = ds
+        self.tokenizer_src=tokenizer_src
+        self.tokenizer_tgt=tokinezer_tgt
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+        self.seq_len = seq_len
+        self.sos_token = torch.tensor([tokenizer_src.token_to_id(['[SOS]'])],dtype=torch.int64)
+        self.eos_token = torch.tensor([tokenizer_src.token_to_id(['[EOS]'])],dtype=torch.int64)
+        self.pad_token = torch.tensor([tokenizer_src.token_to_id(['[PAD]'])],dtype=torch.int64)
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, index):
+        src_target_pair = self.ds[index]
+        src_text = src_target_pair['translation'][self.src_lang]
+        tgt_text = src_target_pair['translation'][self.tgt_lang]
+
+        enc_input_tokens = self.tokenizer_src.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
+        # Why subtract 2 for encoder?
+        # [SOS] and [EOS] tokens are added to the sequence.
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) -2
+        # Why subtract 1 for decoder?
+        # Only [SOS] is added to the input of the decoder (the [EOS] is handled in the label).
+        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) -1
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError(" Sentence is to long ")
+        
+
+        # concat the sos the text and the eos  and pad 
+        # The encoder input is constructed as:
+        # [SOS] + Source Tokens + [EOS] + Padding Tokens
+        # Example:
+        # Input tokens: [10, 5, 7, 22]
+        # Encoder input: [SOS, 10, 5, 7, 22, EOS, PAD, PAD, ...]
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens , dtype=torch.int64),
+                self.eos_token,
+                torch.tensor( [self.pad_token.item()] * enc_num_padding_tokens , dtype=torch.int64)
+            ]
+        )
+
+        ## add sos to the deoder 
+
+        #         The decoder input is constructed as:
+        # [SOS] + Target Tokens + Padding Tokens
+        # Example:
+        # Target tokens: [15, 8, 19, 30]
+        # Decoder input: [SOS, 15, 8, 19, 30, PAD, PAD, ...]
+
+        dec_input = torch.cat([
+            self.sos_token,
+            torch.tensor(dec_input_tokens , dtype=torch.int64),
+            torch.tensor([self.pad_token.item()]* dec_num_padding_tokens , dtype=torch.int64)
+        ])
+
+        ## add sos to the label the output of the decoder 
+        label = torch.cat([
+            self.sos_token,
+            torch.tensor(dec_input_tokens , dtype=torch.int64),
+            torch.tensor([self.pad_token]* dec_num_padding_tokens , dtype=torch.int64)
+        ])
+        assert encoder_input.size(0)==self.seq_len
+        assert dec_input.size(0)==self.seq_len
+        assert label.size(0)==self.seq_len
+        # Encoder Mask (enc_mask):
+        # Prevents attention to padding tokens in the encoder.
+        # Shape: (1, 1, seq_len).
+        # Decoder Mask (dec_mask):
+        # Combines:
+        # Padding mask (to avoid attending to padding tokens).
+        # Causal mask (to ensure tokens can only attend to previous tokens or themselves).
+        # Shape: (1, seq_len, seq_len).
+        return {
+            "encoder_input":encoder_input, # (seq_len)
+            "dec_input":dec_input, # (seq_len)
+            # all the word are pading are ok while not padding not ok 
+            "encd_mask":(encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), # to have (1,1,seq_len) 
+            # each word can only look at the prevouis word and each can only loock not paddding words and not to see the word after it 
+            "dec_mask":(dec_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() & cusal_mask(dec_input.size(0)), # so we have (1,seq) & (1,swq , seq ) so we get (1,seq , seq ) plan of true of false 
+            "label":label ,# (seq),
+            "src_text": src_text,
+            "tgt_text":tgt_text
+        }
+def cusal_mask(size):
+    mask =torch.triu(torch.ones(1 , size , size ) , diagonal=1).type(torch.int)
+    return mask ==0
+    
---
+++ tran.py
@@ -0,0 +1,67 @@
+import torch 
+import torch.nn as nn 
+from datasets import load_dataset
+from torch.utils.data import Dataset , DataLoader , random_split
+## tokenizer to spli the sentenses into many word using many type we gonna work wiyh 
+# the WordLevel and map then into numbers the number of the word im the vocabilure
+
+from tokenizers import Tokenizer
+## tyoe of tokinezer 
+from tokenizers.models import WordLevel
+# trin the tokinezer base on the sentences given 
+from tokenizers.trainers import WordLevelTrainer
+# split the word using spaces 
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+from dataset import BilingualDataset , cusal_mask
+## buil the tokinezer
+def get_or_build_tokenizer(config , ds , lang):
+    tokinezer_path = Path(config["tokinezer_file"].format(lang))
+    # if it not found we creat the tokenization 
+    if not Path.exists(tokinezer_path):
+        # initial the tokenizer by WordLevel method/model 
+        tokenizer = Tokenizer(WordLevel(unk_token="UNK"))
+        # split it using Whitespace
+        tokenizer.pre_tokenizer=Whitespace()
+        # train the split the sentence should have at least two of the splecial tokens 
+        # [UNK]: Unknown token
+        # [PAD]: Padding token for sequences
+        # [SOS]: Start-of-sequence token
+        # [EOS]: End-of-sequence token
+        trainer = WordLevelTrainer(special_tokens=["[UNK]","[PAD]","[SOS]","[EOS]"] , min_frequency=2)
+        # ??
+        tokenizer.train_from_iterator(get_all_sentences(ds,lang),trainer)
+        ## save the tokenzation
+        tokenizer.save(str(tokinezer_path))
+    # if it existe we use it derectely 
+    else:
+        tokenizer= Tokenizer.from_file(str(tokinezer_path))
+    return tokenizer
+
+def get_all_sentences(ds,lang):
+    for items in ds:
+        yield items['translation'][lang]
+
+def get_dataset(config):
+    ds_rows = load_dataset('opus_books', f"{config['lang_src']}-{config['lang_tgt']}", split='train')
+    # build tokenizers for each lang 
+    tokenizer_src = get_or_build_tokenizer(config , ds_rows , config["lang_src"])
+    tokenizer_tgt = get_or_build_tokenizer(config , ds_rows , config["lang_tgt"])
+
+    # split the data into validation and tain 
+    train_ds_size = int(0.9 * len(ds_rows))
+    val_ds_size = int(0.1 * len(ds_rows))
+    train_ds , val_ds = random_split(ds_rows , [train_ds_size,val_ds_size])
+    train_ds = BilingualDataset(train_ds,tokenizer_src,tokenizer_tgt,config["lang_src"],config["lang_tgt"],config["seq_len"])
+    val_ds = BilingualDataset(val_ds,tokenizer_src,tokenizer_tgt,config["lang_src"],config["lang_tgt"],config["seq_len"])
+    max_len_src=0
+    max_len_tgt=0
+    for item in ds_rows:
+        max_len_src=max(max_len_src, tokenizer_src.encode(item["translation"][config["lang_src"]]))
+        max_len_tgt=max(max_len_tgt, tokenizer_src.encode(item["translation"][config["lang_tgt"]]))
+    print(f'max lenght in the src langague is {max_len_src}')
+    print(f'max lenght in the tgt langague is {max_len_tgt}')
+    
+    # return train_ds, val_ds, tokenizer_src, tokenizer_tgt
